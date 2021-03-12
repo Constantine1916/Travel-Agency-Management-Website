559538db7a39e5c2ed936ca6db196bf7c0d8d0e9
Constantine <862947199@qq.com>
2021-03-12 14:02:53 +0800

    去掉头部注释

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,3 @@
-/*
- * @Description  : 
- * @Author       : sunjr
- * @Date         : 2021-03-12 12:33:22
- * @LastEditors  : sunjr
- * @LastEditTime : 2021-03-12 12:42:22
- * @FilePath     : \travel-agency-management-website\src\router\index.js
- */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
