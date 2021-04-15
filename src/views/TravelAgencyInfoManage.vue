2dc6465efb69d4eb24e2030bbdd705a5a44965bf
Constantine <862947199@qq.com>
2021-04-15 19:46:31 +0800

    新增表格

--- src/views/TravelAgencyInfoManage.vue
+++ src/views/TravelAgencyInfoManage.vue
@@ -3,7 +3,7 @@
  * @Author       : sunjr
  * @Date         : 2021-03-12 12:33:22
  * @LastEditors  : sunjr
- * @LastEditTime : 2021-04-15 17:42:08
+ * @LastEditTime : 2021-04-15 18:03:54
  * @FilePath     : \travel-agency-management-website\src\views\TravelAgencyInfoManage.vue
 -->
 <template>
@@ -12,19 +12,94 @@
       <SearchComponent @search="handleSearch"></SearchComponent>
     </div>
     <div class="table">
-
+      <a-table :columns="tableColumns" :data-source="tableData">
+        <a slot="name" slot-scope="text">{{ text }}</a>
+        <span slot="customTitle"><a-icon type="home" /> Name</span>
+        <span slot="tags" slot-scope="tags">
+          <a-tag
+            v-for="tag in tags"
+            :key="tag"
+            :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+          >
+            {{ tag.toUpperCase() }}
+          </a-tag>
+        </span>
+        <span slot="action" slot-scope="text, record">
+          <a>Invite 一 {{ record.name }}</a>
+          <a-divider type="vertical" />
+          <a>Delete</a>
+          <a-divider type="vertical" />
+          <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
+        </span>
+      </a-table>
     </div>
   </div>
 </template>
 <script>
 import SearchComponent from "../components/SearchComponent"
+
+const tableColumns = [
+  {
+    dataIndex: 'name',
+    key: 'name',
+    slots: { title: 'customTitle' },
+    scopedSlots: { customRender: 'name' },
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    scopedSlots: { customRender: 'tags' },
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    scopedSlots: { customRender: 'action' },
+  },
+];
+
+const tableData = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
 export default {
   components: {
     SearchComponent
   },
   data() {
     return {
-      
+      tableColumns,
+      tableData,
     }
   },
   methods: {
@@ -37,6 +112,10 @@ export default {
 <style lang="scss">
 .travelAgencyInfoManage {
   .searchContainer {
+    padding: 10px;
+  }
+  .table {
+    width: 100%;
     padding: 10px;
   }
 }
