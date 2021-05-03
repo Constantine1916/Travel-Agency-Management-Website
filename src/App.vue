8c05f9a9da8dd5c3eb66337b8110af56c9ab92fc
Constantine <862947199@qq.com>
2021-05-03 12:19:58 +0800

    把vuex/state中的id存入sessionstorage中 解决刷新页面id丢失的问题

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
+<!--
+ * @Description  : 
+ * @Author       : sunjr
+ * @Date         : 2021-03-12 10:43:21
+ * @LastEditors  : sunjr
+ * @LastEditTime : 2021-05-03 12:15:19
+ * @FilePath     : \travel-agency-management-website\src\App.vue
+-->
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -28,3 +36,24 @@
   }
 }
 </style>
+<script>
+export default {
+  created() {
+    //在页面加载时读取sessionStorage里的状态信息
+    if (sessionStorage.getItem('store')) {
+      this.$store.replaceState(
+        Object.assign(
+          {},
+          this.$store.state,
+          JSON.parse(sessionStorage.getItem('store'))
+        )
+      )
+    }
+
+    //在页面刷新时将vuex里的信息保存到sessionStorage里
+    window.addEventListener('beforeunload', () => {
+      sessionStorage.setItem('store', JSON.stringify(this.$store.state))
+    })
+  }
+}
+</script>
